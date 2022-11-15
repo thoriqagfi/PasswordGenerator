3f376ac4be4a1bef9a2b09af2a042ea1d8d9a32c
Thariq Agfi Hermawan <thoriq.aghfi60@gmail.com>
2022-11-15 20:43:08 +0700

    initial commit

---
+++ PasswordGenerator.py
@@ -0,0 +1,15 @@
+import random
+
+passwordGenerator = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?', '/', '`', '~'
+]
+
+password = ""
+
+for i in range(16):
+  password = password + random.choices(passwordGenerator)[0]
+
+print("Your new password is: \n" + password)
---
+++ README.md
@@ -0,0 +1,12 @@
+# Password Generator
+
+This is a simple way to create a Password Generator.
+
+### Default
+
+**Default Length:** 16 Letters
+
+### Authors
+
+- [Agfi](https://www.github.com/thoriqagfi)
+
